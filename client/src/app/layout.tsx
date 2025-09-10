dc0bc7583579ea86aeb62a42b3085b8af1240d61
ahmed nabil <am4201828@gmail.com>
2025-09-10 22:25:20 +0300

    finish home

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,12 @@
-import NavBar from "@/components/NavBar";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={kanit.className}>
       <body>
         <NavBar />
         <main>{children}</main>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="sectionsContainer">
       <section id="Home" className="homeSection">
-
+        <div className="home_header">
+          <h1>Your virtual assistant for instant</h1>
+          <h3>personalized website support.</h3>
+          <Link href={"/login"}>Get Started</Link>
+        </div>
+        <div className="home_imgs">
+          <img className="img1" src="/assets/robot.png" alt="" />
+          <img className="img2" src="/assets/Component.png" alt="" />
+        </div>
       </section>
-      <section id="aboutUs" className="aboutUsSection">
-        
-      </section>
-      <section id="contactUs" className="contactUsSection">
-        
-      </section>
+      <section id="aboutUs" className="aboutUsSection"></section>
+      <section id="contactUs" className="contactUsSection"></section>
     </div>
   );
 }
