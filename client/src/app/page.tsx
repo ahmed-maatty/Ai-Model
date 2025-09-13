58688b84fb6fbcec84431e40a5bfe28a3fe2ed6a
ahmed nabil <am4201828@gmail.com>
2025-09-13 12:50:11 +0300

    about us

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -14,7 +14,39 @@ export default function Home() {
           <img className="img2" src="/assets/Component.png" alt="" />
         </div>
       </section>
-      <section id="aboutUs" className="aboutUsSection"></section>
+      <section id="aboutUs" className="aboutUsSection">
+        <div className="aboutHeader">
+          <h1>Why US</h1>
+          <p>
+            Choose us for unparalleled expertise, tailored solutions, and
+            cutting-edge technology. Join a community of satisfied clients who
+            thrive with our customer-centric approach and proven track record of
+            success.
+          </p>
+        </div>
+        <div className="why_us_boxes">
+          <div className="why_us_2_boxes">
+            <div className="box">
+              <div className="bg_container">
+                <img className="img_lamb" src="/assets/lamb.png" alt="" />
+              </div>
+              <h5>Expertise and Innovation</h5>
+            </div>
+            <div className="box">
+              <div className="bg_container">
+                <img className="diamond_img" src="/assets/diamond.png" alt="" />
+              </div>
+              <h5>Client-Centric Excellence</h5>
+            </div>
+          </div>
+          <div className="box">
+            <div className="bg_container">
+                <img className="puzzle_img" src="/assets/puzzle.png" alt="" />
+              </div>
+              <h5>Tailored Solutions</h5>
+          </div>
+        </div>
+      </section>
       <section id="contactUs" className="contactUsSection"></section>
     </div>
   );
