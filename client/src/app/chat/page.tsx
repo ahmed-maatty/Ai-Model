ce10ec139dfad6ba402f60a4b9baefd0c9091ddd
ahmed nabil <am4201828@gmail.com>
2025-09-26 22:31:10 +0300

    not responsive

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,18 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-function page() {
+function Chat() {
+  const [message , setMessage] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const chatHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:8000/chat/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({prompt}),
+      });
+      const data = await res.json();
+      setMessage(data.result);
+      document.getElementById("messageBar").value = " "
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section className="chatSection">
       <div className="chatScreen">
         <h1>oscar</h1>
-        <div className="screen"></div>
+        <div className="screen">
+          {
+            prompt && (
+              <div className="prompt_contaiener">
+                <p className="sender">{prompt}</p>
+              </div>
+            )
+          }
+          {
+            message && (
+              <div className="prompt_contaiener">
+                <p className="response">{message}</p>
+              </div>
+            )
+          }
+        </div>
         <div className="input_btn_send">
-          <input type="text" placeholder="Write Your Message" />
-          <button>Send</button>
+          <input
+            type="text"
+            placeholder="Write Your Message"
+            onChange={(e) => setPrompt(e.target.value)}
+            id="messageBar"
+          />
+          <button onClick={chatHandler}>Send</button>
         </div>
       </div>
     </section>
   );
 }
 
-export default page;
+export default Chat;
